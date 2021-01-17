f0a875d04887f5b4f0da3d3a5f4915b081d4d7b8
emacsbliss <emacsbliss@gmail.com>
2021-01-17 10:42:54 -0500

    [emacs-reveal] add package

--- modules/my/dired/config.el
+++ modules/my/dired/config.el
@@ -1,4 +1,4 @@
-;; [[file:../../../../../../var/folders/qt/lhqmcxqs5lnfgst5md2t8tfr0000gn/T/config.org.61j08Y::*dired+][dired+:2]]
+;; [[file:../../../../../../var/folders/v5/8_bvmqtj6nn2jd8l581ylfb80000gp/T/config.org.pELcr6::*dired+][dired+:2]]
 (use-package! dired+)
 ;; I want to see the details
 (add-hook 'dired-mode-hook
@@ -6,20 +6,20 @@
               (dired-hide-details-mode -1)))
 ;; dired+:2 ends here
 
-;; [[file:../../../../../../var/folders/qt/lhqmcxqs5lnfgst5md2t8tfr0000gn/T/config.org.61j08Y::*ranger][ranger:1]]
+;; [[file:../../../../../../var/folders/v5/8_bvmqtj6nn2jd8l581ylfb80000gp/T/config.org.pELcr6::*ranger][ranger:1]]
 (setq ranger-override-dired-mode nil)
 (setq ranger-override-dired nil)
 ;; ranger:1 ends here
 
-;; [[file:../../../../../../var/folders/qt/lhqmcxqs5lnfgst5md2t8tfr0000gn/T/config.org.61j08Y::*config][config:1]]
+;; [[file:../../../../../../var/folders/v5/8_bvmqtj6nn2jd8l581ylfb80000gp/T/config.org.pELcr6::*config][config:1]]
 (setq dired-recursive-deletes 'always)
 ;; config:1 ends here
 
-;; [[file:../../../../../../var/folders/qt/lhqmcxqs5lnfgst5md2t8tfr0000gn/T/config.org.61j08Y::*config][config:2]]
+;; [[file:../../../../../../var/folders/v5/8_bvmqtj6nn2jd8l581ylfb80000gp/T/config.org.pELcr6::*config][config:2]]
 (setq dired-listing-switches "-aBhl")
 ;; config:2 ends here
 
-;; [[file:../../../../../../var/folders/qt/lhqmcxqs5lnfgst5md2t8tfr0000gn/T/config.org.61j08Y::*config][config:3]]
+;; [[file:../../../../../../var/folders/v5/8_bvmqtj6nn2jd8l581ylfb80000gp/T/config.org.pELcr6::*config][config:3]]
 (map! :map dired-mode-map
   :desc "open"  :n "z" #'me/mac-open)
 ;; config:3 ends here
